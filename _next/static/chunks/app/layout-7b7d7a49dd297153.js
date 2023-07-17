163f0f879999f0e865cfe608ce3fc1ad74987bbe
hnnazm <work@hnnazm.com>
2023-07-18 03:14:04 +0800

    Rewrite using Next

---
+++ _next/static/chunks/app/layout-7b7d7a49dd297153.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3155:function(n,e,u){Promise.resolve().then(u.t.bind(u,54,23))},54:function(){}},function(n){n.O(0,[253,961,744],function(){return n(n.s=3155)}),_N_E=n.O()}]);
